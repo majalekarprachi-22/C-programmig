f72fe45cf485ad634730217cc8d7a49dad441694
Prachi Majalekar <majalekarprachi@gmail.com>
2026-08-02 10:58:00 +0530

    Update even_odd_numbers.c

--- even_odd_numbers.c
+++ even_odd_numbers.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main() 
+{ int num,even,odd;
+ printf("Enter a  number:");
+ scanf("%d",&num);
+ if(num%2==0)
+ printf("%d is even",num);
+ else
+ printf("%d is odd",num);
+    
 
+    return 0;
+}
