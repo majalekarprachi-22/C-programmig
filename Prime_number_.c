ddc1560b91641899cea86cb8dbcddfe1251de30d
Prachi Majalekar <majalekarprachi@gmail.com>
2026-08-05 19:13:02 +0530

    Add prime number checking program in C

---
+++ Prime_number_.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i, prime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1)
+    {
+        prime = 0;
+    }
+    else
+    {
+        for (i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (prime == 1)
+        printf("%d is a Prime Number.", num);
+    else
+        printf("%d is not a Prime Number.", num);
+
+    return 0;
+}
