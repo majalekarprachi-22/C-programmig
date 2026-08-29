596b3338668240e4ce6d958d1284b4dc0b6fe385
Prachi Majalekar <majalekarprachi@gmail.com>
2026-08-29 12:51:18 +0530

    Implement digit summation for five-digit number
    Fix the logic to sum the digits of a five-digit number.

---
+++ sum_of_five_digit_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+  int num,sum=0
+
+  printf("Enter the five digit number:");
+  scanf("%d",&num);
+
+  while(num < 0)
+    {
+      sum = sum + num % 10;
+      num = num / 10;
+    }
+  printf("sum = %d", sum);
+  return 0;
+}
