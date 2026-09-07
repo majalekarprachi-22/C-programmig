a20a5c8ff569f5555ca7ecd13a2226656ab0c0d1
Prachi Majalekar <majalekarprachi@gmail.com>
2026-09-07 07:23:24 +0530

    Implement factorial calculation using while loop

---
+++ factorialwhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i = 1, fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(i <= n)
+    {
+        fact = fact * i;
+        i++;
+    }
+
+    printf("Factorial = %d", fact);
+
+    return 0;
+}
